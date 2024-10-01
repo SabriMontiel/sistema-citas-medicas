b4401916d591628b65fd8ce16995a67e7e037b9d
Lucas Scherf <62562611+SDLucas@users.noreply.github.com>
2024-10-01 20:25:26 -0300

    Update sistema_citas.sql
    agregadas tablas de profesional, asignacion_profesional_paciente, provincia y ciudad, y agregado IDENTITY(1,1) a las id correspondientes

--- sistema_citas.sql
+++ sistema_citas.sql
@@ -2,37 +2,48 @@ CREATE DATABASE sistema_citas;
 
 USE sistema_citas;
 
--- Tabla Localidad
-CREATE TABLE Localidad (
-    id_localidad INT,
-    nombre VARCHAR(100) NOT NULL,
-    CONSTRAINT PK_localidad PRIMARY KEY (id_localidad)
+
+CREATE TABLE Provincia (
+    id_provincia INT IDENTITY(1,1),
+    nombre_provincia VARCHAR(30) NOT NULL,
+     CONSTRAINT PK_Provincial PRIMARY KEY (id_provincia)
 );
 
---Tabla código postal
-CREATE TABLE Codigo_postal (
-    id_cod_postal INT ,
-    num_codigo VARCHAR(10) NOT NULL,
-     CONSTRAINT PK_codigoPostal PRIMARY KEY (id_cod_postal)
+CREATE TABLE Ciudad (
+    id_ciudad INT IDENTITY(1,1),
+    nombre_ciudad VARCHAR(30) NOT NULL,
+	id_provincia INT not null,
+     CONSTRAINT PK_Ciudad PRIMARY KEY (id_ciudad),
+	 CONSTRAINT FK_Ciudad_Provincia FOREIGN KEY (id_provincia) REFERENCES Provincia(id_provincia)
+);
+
+-- Tabla Localidad
+CREATE TABLE Localidad (
+    id_localidad INT IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+	cod_postal int not null,
+	id_ciudad int not null,
+    CONSTRAINT PK_localidad PRIMARY KEY (id_localidad),
+	CONSTRAINT FK_Localidad_Ciudad FOREIGN KEY 
 );
 
 --Tabla sexo
 CREATE TABLE Sexo (
-    id_sexo INT ,
+    id_sexo INT IDENTITY(1,1),
     descripcion VARCHAR(50) NOT NULL,
      CONSTRAINT PK_sexo PRIMARY KEY (id_sexo)
 );
 
 --Tabla obra social
 CREATE TABLE Obra_social (
-    id_obra_social INT ,
+    id_obra_social INT IDENTITY(1,1),
     descripcion VARCHAR(100) NOT NULL,
      CONSTRAINT PK_obraSocial PRIMARY KEY (id_obra_social)
 );
 
 --Tabla paciente
 CREATE TABLE Paciente (
-    id_paciente INT ,
+    id_paciente INT IDENTITY(1,1),
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
     domicilio VARCHAR(255),
@@ -45,8 +56,7 @@ CREATE TABLE Paciente (
     id_sexo INT,
     id_obra_social INT,
     CONSTRAINT PK_paciente PRIMARY KEY (id_paciente),
-    CONSTRAINT FK_paciente_localidad FOREIGN KEY (id_localidad) REFERENCES Localidad(id_localidad),
-    CONSTRAINT PK_paciente_cp FOREIGN KEY (id_cod_postal) REFERENCES Codigo_postal(id_cod_postal),
+    CONSTRAINT FK_paciente_Ciudad FOREIGN KEY (id_localidad) REFERENCES Ciudad(id_ciudad),
     CONSTRAINT PK_paciente_sexo FOREIGN KEY (id_sexo) REFERENCES Sexo(id_sexo),
     CONSTRAINT PK_paciente_obraSocial FOREIGN KEY (id_obra_social) REFERENCES Obra_social(id_obra_social),
     CONSTRAINT UK_paciente_email UNIQUE(email),
@@ -60,7 +70,7 @@ CREATE TABLE Paciente (
 
 CREATE TABLE Tratamiento
 (
-  id_tratamiento INT NOT NULL,
+  id_tratamiento INT IDENTITY(1,1) NOT NULL,
   descripcion VARCHAR(40) NOT NULL,
   CONSTRAINT PK_Tratamiento PRIMARY KEY (id_tratamiento)
 );
@@ -79,5 +89,24 @@ CREATE TABLE Atencion_medica_diagnostico
   CONSTRAINT PK_Atencion_medica_diagnostico PRIMARY KEY (id_diagnostico, id_atencion_medica),
   CONSTRAINT FK_Atencion_medica_diagnostico_REFERENCES_Diagnostico FOREIGN KEY (id_diagnostico) REFERENCES Diagnostico(id_diagnostico),
   CONSTRAINT FK_Atencion_medica_diagnostico_REFERENCES_Atencion_medica FOREIGN KEY (id_atencion_medica) REFERENCES Atencion_medica(id_atencion_medica)
+);
+
+CREATE TABLE Profesional (
+    id_profesional INT IDENTITY(1,1),
+    nombre_profesional VARCHAR(30) NOT NULL,
+	apellido_profesional VARCHAR(30) NOT NULL,
+	lic_medica INT NOT NULL,
+    CONSTRAINT PK_Profesional PRIMARY KEY (id_profesional),
+	CONSTRAINT UQ_Profesional_lic_medica UNIQUE(lic_medica)
+);
+
+CREATE TABLE Asignacion_profesional_paciente (
+    id_paciente INT,
+	id_profesional INT,
+    fecha_inicio DATE NOT NULL,
+	fecha_fin DATE NOT NULL,
+    CONSTRAINT PK_Profesional PRIMARY KEY (id_paciente,id_profesional),
+	CONSTRAINT FK_Asignacion_profesional_paciente_paciente FOREIGN KEY (id_paciente) REFERENCES Paciente(id_paciente),
+	CONSTRAINT FK_Asignacion_profesional_paciente_profesional FOREIGN KEY (id_profesional) REFERENCES Profesional(id_profesional)
 );
 
