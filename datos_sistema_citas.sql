de5b545044ca52cba8a0a5aa9b861e4a83171e1e
fernandoromero0 <feernandoroomero@gmail.com>
2024-10-02 14:18:58 -0300

    Update datos_sistema_citas.sql
    arreglo de el: Lote de datos representativos

--- datos_sistema_citas.sql
+++ datos_sistema_citas.sql
@@ -1,4 +1,3 @@
-
 USE sistema_citas;
 GO
 -- Lote de datos representativos--
@@ -91,6 +90,16 @@ INSERT INTO Diagnostico (id_diagnostico,descripcion) VALUES
 (6,'Síndrome del túnel carpiano');
 GO
 
+
+INSERT INTO Atencion_medica (id_atencion_medica, fecha_consulta,notas_observacion_, id_paciente, id_profesional, id_tratamiento) VALUES
+(1, '2024-01-10', 'Paciente con dolor muscular en el brazo derecho.', 1, 1, 1),
+(2, '2024-02-14', 'Paciente con inflamación en la rodilla.', 2, 2, 2),
+(3, '2024-03-03', 'Paciente con fatiga crónica.', 3, 3, 3),
+(4, '2024-04-22', 'Paciente con dolor en el cuello.', 4, 4, 4),
+(5, '2024-05-12', 'Paciente en rehabilitación por fractura de brazo.', 5, 5, 5),
+(6, '2024-06-07', 'Paciente con síndrome del túnel carpiano.', 6, 6, 6);
+
+
 INSERT INTO Atencion_medica_diagnostico (id_diagnostico, id_atencion_medica) VALUES 
 (1, 1),
 (2, 1),
@@ -100,13 +109,6 @@ INSERT INTO Atencion_medica_diagnostico (id_diagnostico, id_atencion_medica) VAL
 (6, 2);
 GO
 
-INSERT INTO Atencion_medica (id_atencion_medica, fecha_consulta,notas_observacion_, id_paciente, id_profesional, id_tratamiento) VALUES
-(1, '2024-01-10', 'Paciente con dolor muscular en el brazo derecho.', 1, 1, 1),
-(2, '2024-02-14', 'Paciente con inflamación en la rodilla.', 2, 2, 2),
-(3, '2024-03-03', 'Paciente con fatiga crónica.', 3, 3, 3),
-(4, '2024-04-22', 'Paciente con dolor en el cuello.', 4, 4, 4),
-(5, '2024-05-12', 'Paciente en rehabilitación por fractura de brazo.', 5, 5, 5),
-(6, '2024-06-07', 'Paciente con síndrome del túnel carpiano.', 6, 6, 6);
 
 INSERT INTO Ficha_Paciente (id_paciente, id_atencion_medica) VALUES
 (1, 1),
